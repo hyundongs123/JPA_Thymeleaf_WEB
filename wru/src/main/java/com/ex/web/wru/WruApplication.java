b16d0e4aa08b7a5b17db3716dccefcb004295c58
hyundong <hyundong456@gmail.com>
2024-09-19 22:28:15 +0900

    Initial commit

---
+++ wru/src/main/java/com/ex/web/wru/WruApplication.java
@@ -0,0 +1,14 @@
+package com.ex.web.wru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class WruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WruApplication.class, args);
+	}
+
+}
---
+++ wru/src/test/java/com/ex/web/wru/WruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.web.wru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
